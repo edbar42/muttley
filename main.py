8ba89cc0bd91d8eb54ada7381b5b28082f253ab4
Eduardo Barroso <dev@edbar.xyz>
2025-06-26 05:32:42 -0300

    feat: prompt to llm received as arguments to the script

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -6,14 +7,19 @@ load_dotenv()
 api_key = os.environ.get("API_KEY")
 client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
+try:
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=sys.argv[1]
+    )
 
-print(
-    f"Prompt tokens: {response.usage_metadata.prompt_token_count}",
-    f"Response tokens: {response.usage_metadata.candidates_token_count}",
-    response.text,
-    sep="\n"
-)
+    print(
+        f"Prompt tokens: {response.usage_metadata.prompt_token_count}",
+        f"Response tokens: {response.usage_metadata.candidates_token_count}",
+        response.text,
+        sep="\n"
+    )
+
+except IndexError:
+    print("No argument was given. Try again.")
+    exit(1)
