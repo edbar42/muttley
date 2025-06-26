b68e9c1b314cdfe6a97edd534b210c0a48b5427d
Eduardo Barroso <dev@edbar.xyz>
2025-06-26 05:55:00 -0300

    feat: add verbose flag

--- main.py
+++ main.py
@@ -5,26 +5,38 @@ from google import genai
 from google.genai import types
 
 
-load_dotenv()
-api_key = os.environ.get("API_KEY")
-client = genai.Client(api_key=api_key)
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <prompt>")
+        print("Try passing in a message in double quotes when you call this script.")
+        sys.exit(1)
 
-try:
+    prompt = sys.argv[1]
+
+    load_dotenv()
+    api_key = os.environ.get("API_KEY")
+    client = genai.Client(api_key=api_key)
+
     messages = [
-        types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
     ]
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages
     )
 
-    print(
-        f"Prompt tokens: {response.usage_metadata.prompt_token_count}",
-        f"Response tokens: {response.usage_metadata.candidates_token_count}",
-        response.text,
-        sep="\n"
-    )
+    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+        print(
+            f"User prompt: {messages[-1].parts[-1].text}",
+            f"Prompt tokens: {response.usage_metadata.prompt_token_count}",
+            f"Response tokens: {
+                response.usage_metadata.candidates_token_count}",
+            response.text,
+            sep="\n"
+        )
+    else:
+        print(response.text)
 
-except IndexError:
-    print("No argument was given. Try again.")
-    exit(1)
+
+if __name__ == "__main__":
+    main()
