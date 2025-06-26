ebc2ec71f5765b2ce5d02d04f2121f0e34d5ba77
Eduardo Barroso <dev@edbar.xyz>
2025-06-26 05:39:09 -0300

    chore: add messages list to have a context window available to the llm

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
 
 load_dotenv()
 api_key = os.environ.get("API_KEY")
 client = genai.Client(api_key=api_key)
 
 try:
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
+    ]
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=sys.argv[1]
+        contents=messages
     )
 
     print(
